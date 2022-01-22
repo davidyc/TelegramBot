a5f30d86ffc05c586bdc51f33c755354a975c5ef
davidyc1 <Sergey_Davydov2@EPKZKARW0882>
2022-01-22 13:46:50 +0600

    add first working version

--- TelegramBot/TelegramBotConsole/Program.cs
+++ TelegramBot/TelegramBotConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TelegramBotLibrary.Implementation;
 
 namespace TelegramBotConsole
 {
@@ -6,7 +7,8 @@ namespace TelegramBotConsole
     {
         static void Main(string[] args)
         {
-            var bot = new TelegramBotLibrary.TelegramBot("");
+            var bot = new TelegramBot("5015727824:AAHk4W_vg2GIXk6f0Yx3krol6DGl-UE5rMc");
+            bot.Run();
             Console.WriteLine("Hello World!");
         }
     }
